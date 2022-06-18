9e4229ca9e1a73af51a5e3c3302208be2e9871ab
Dardanby <71726386+Dardanby@users.noreply.github.com>
2022-06-18 23:23:13 +0200

    Add files via upload

---
+++ config.js
@@ -0,0 +1,14 @@
+tailwind.config = {
+    theme: {
+      extend: {
+        colors: {
+          blu: '#A2D2FF',
+          azzurro: '#BDE0FE',
+          rosa: '#FFAFCC',
+          rosaChiaro: '#FFC8DD',
+          viola: '#CDB4DB',
+          verde: '#9bd2ca',
+        }
+      }
+    }
+  }
